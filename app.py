cf3964ad702645b2e1f24a2fd51cf13a136b594f
avanisharma-5 <avanis0501@gmail.com>
2026-04-21 20:19:52 +0530

    replica testing

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 # Page config
 st.set_page_config(page_title="Insurance System", page_icon="💬")
 
-st.title("💬 Insurance Assistant latest")
+st.title("💬 Insurance Assistant v1")
 st.write("Ask anything about insurance policies, claims, coverage, etc.")
 st.write("this is new version!")
 
